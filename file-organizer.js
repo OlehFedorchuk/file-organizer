7584f67a967ed584f09929ad70b35602bd0ce8cd
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2026-07-13 11:42:16 +0100

    added main file

---
+++ file-organizer.js
@@ -0,0 +1,34 @@
+import scan from "./lib/scanner.js";
+import duplicates from "./lib/duplicates.js";
+import cleanup from "./lib/cleanup.js";
+import organaizer from "./lib/organizer.js";
+import fs from "node:fs/promises";
+
+function main(arg, path) {
+  if (!path) {
+    console.log(
+      "Error! Not entered path to derectiry, use: node analyzer.js <path>",
+    );
+    return 0;
+  }
+  switch (arg) {
+    case "scan":
+      console.log("Func scan", scan(path));
+      break;
+    case "duplicates":
+      console.log(duplicates(path));
+      break;
+    case "cleanup":
+      console.log("Func cleanup", cleanup(path));
+      break;
+    case "organize":
+      console.log("Func organize", organaizer(path));
+      break;
+
+    default:
+      break;
+  }
+}
+
+const [, , arg, path] = process.argv;
+main(arg, path);
